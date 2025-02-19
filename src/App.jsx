3ce0e251a6ef122a0feda021b8397ebd94a28edd
mubina <mubina@gmail.com>
2025-02-19 19:56:34 +0500

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
       children: [
         {
           index: true,
+          path: "/",
           element: <Home />,
-          path: "/home",
         },
         {
           path: "/list",
